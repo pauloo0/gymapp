f76bf82e5b88eeaa21c705aa0b5d434e2da9810c
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2024-08-09 21:11:49 +0100

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <div className='flex items-center justify-center w-full h-screen'>
+      <Button>Click Me!</Button>
+    </div>
+  )
+}
+
+export default App
