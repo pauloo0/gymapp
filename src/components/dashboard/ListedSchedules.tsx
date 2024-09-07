b8d19b07258ea0ac029e456ca6c5e5e452905cd0
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2024-09-07 22:26:43 +0100

    Add message to listed schedules when there are none

--- src/components/dashboard/ListedSchedules.tsx
+++ src/components/dashboard/ListedSchedules.tsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
 
 interface Schedule {
   id: string
@@ -79,7 +80,7 @@ function ListedSchedules() {
   }, [token])
 
   const navigateToSchedule = (id: string) => {
-    window.location.href = `/schedule/${id}`
+    window.location.href = `/marcacao/${id}`
   }
 
   return (
@@ -88,21 +89,32 @@ function ListedSchedules() {
         <CardTitle>Agendamentos</CardTitle>
       </CardHeader>
       <CardContent className='grid grid-cols-1 gap-2 p-2'>
-        {schedules.map((schedule: Schedule) => (
-          <div
-            className='flex flex-row items-center justify-between px-4 py-2 border rounded-xl hover:cursor-pointer hover:bg-slate-100'
-            key={schedule.id}
-            onClick={() => navigateToSchedule(schedule.id)}
-          >
-            <div>
-              {schedule.clients.firstname + ' ' + schedule.clients.lastname}
-            </div>
-            <div className='flex flex-col items-end justify-center text-sm text-slate-500'>
-              <span>{schedule.time}</span>
-              <span>{new Date(schedule.date).toLocaleDateString('pt-PT')}</span>
-            </div>
+        {schedules.length === 0 ? (
+          <div className='flex flex-col justify-center gap-2'>
+            <p className='text-center'>Sem marcações</p>
+            <Button onClick={() => navigateToSchedule('novo')}>
+              Fazer marcação
+            </Button>
           </div>
-        ))}
+        ) : (
+          schedules.map((schedule: Schedule) => (
+            <div
+              className='flex flex-row items-center justify-between px-4 py-2 border rounded-xl hover:cursor-pointer hover:bg-slate-100'
+              key={schedule.id}
+              onClick={() => navigateToSchedule(schedule.id)}
+            >
+              <div>
+                {schedule.clients.firstname + ' ' + schedule.clients.lastname}
+              </div>
+              <div className='flex flex-col items-end justify-center text-sm text-slate-500'>
+                <span>{schedule.time}</span>
+                <span>
+                  {new Date(schedule.date).toLocaleDateString('pt-PT')}
+                </span>
+              </div>
+            </div>
+          ))
+        )}
       </CardContent>
     </Card>
   )
