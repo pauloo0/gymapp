34da6824cbf4cd7d8454aec928c68acbdefb994f
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2024-09-20 21:30:50 +0100

    Style invoices section in client profile

--- src/pages/Client Profile/ClientInvoices.tsx
+++ src/pages/Client Profile/ClientInvoices.tsx
@@ -1,15 +1,77 @@
 import { Invoice } from '@/utils/interfaces'
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
 function ClientInvoices({ invoices }: { invoices: Invoice[] }) {
+  const navigateTo = (path: string) => {
+    window.location.href = path
+  }
+
+  const orderedInvoices = invoices
+    .sort((invoiceA: Invoice, invoiceB: Invoice) => {
+      const statusOrder: { [key in string]: number } = {
+        overdue: 1,
+        partial_paid: 2,
+        issued: 3,
+        paid: 4,
+      }
+
+      return statusOrder[invoiceA.status] - statusOrder[invoiceB.status]
+    })
+    .map((invoice: Invoice) => {
+      switch (invoice.status) {
+        case 'overdue':
+          invoice.status = 'Vencido'
+          break
+        case 'partial_paid':
+          invoice.status = 'Parcial'
+          break
+        case 'issued':
+          invoice.status = 'Emitido'
+          break
+        case 'paid':
+          invoice.status = 'Pago'
+          break
+      }
+
+      return invoice
+    })
+
   return (
-    <>
-      {invoices.map((invoice) => (
-        <div key={invoice.id}>
-          {invoice.issue_date} - {invoice.due_date} | {invoice.amount} € |{' '}
-          {invoice.status}
-        </div>
-      ))}
-    </>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className='p-2'>Data</TableHead>
+          <TableHead className='p-2'>Vencimento</TableHead>
+          <TableHead className='p-2'>Valor €</TableHead>
+          <TableHead className='p-2'>Estado</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {orderedInvoices.map((invoice) => (
+          <TableRow
+            key={invoice.id}
+            onClick={() => navigateTo(`/faturas/${invoice.id}`)}
+          >
+            <TableCell className='p-2'>
+              {new Date(invoice.issue_date).toLocaleDateString('pt-PT')}
+            </TableCell>
+            <TableCell className='p-2'>
+              {new Date(invoice.due_date).toLocaleDateString('pt-PT')}
+            </TableCell>
+            <TableCell className='p-2'>{invoice.amount}</TableCell>
+            <TableCell className='p-2'>{invoice.status}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
 
