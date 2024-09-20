cab6497c715f8c970266d385716031b6f5da2a4e
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2024-09-20 20:11:22 +0100

    Style workout section in client profile

--- src/pages/Client Profile/ClientWorkouts.tsx
+++ src/pages/Client Profile/ClientWorkouts.tsx
@@ -1,12 +1,49 @@
 import { Workout } from '@/utils/interfaces'
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Button } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
+
 function ClientWorkouts({ workouts }: { workouts: Workout[] }) {
+  const navigateTo = (path: string) => {
+    window.location.href = path
+  }
+
   return (
-    <>
-      {workouts.map((workout) => (
-        <div key={workout.id}>{workout.name}</div>
-      ))}
-    </>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className='p-2'>Nome</TableHead>
+          <TableHead className='p-2'>Ativo</TableHead>
+          <TableHead className='p-2'></TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {workouts.map((workout) => (
+          <TableRow
+            key={workout.id}
+            onClick={() => navigateTo(`/treinos/${workout.id}`)}
+          >
+            <TableCell className='p-2'>{workout.name}</TableCell>
+            <TableCell className='p-2'>
+              {workout.active ? 'Sim' : 'Não'}
+            </TableCell>
+            <TableCell className='p-2'>
+              <Button className='bg-transparent text-slate-400'>
+                <ArrowRight></ArrowRight>
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
 
