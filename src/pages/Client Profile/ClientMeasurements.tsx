b1d089acba8046f32e392cca65faf8f07e969593
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2024-09-20 19:59:51 +0100

    Style measurement section in client profile

--- src/pages/Client Profile/ClientMeasurements.tsx
+++ src/pages/Client Profile/ClientMeasurements.tsx
@@ -1,12 +1,45 @@
 import { Measurement } from '@/utils/interfaces'
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Button } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
+
 function ClientMeasurements({ measurements }: { measurements: Measurement[] }) {
+  const navigateTo = (path: string) => {
+    window.location.href = path
+  }
+
   return (
-    <>
-      {measurements.map((measurement) => (
-        <div key={measurement.id}>{measurement.date}</div>
-      ))}
-    </>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Data</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {measurements.map((measurement) => (
+          <TableRow>
+            <TableCell
+              key={measurement.id}
+              onClick={() => navigateTo(`/avaliacoes/${measurement.id}`)}
+              className='flex items-center justify-between cursor-pointer'
+            >
+              {new Date(measurement.date).toLocaleDateString('pt-PT')}
+              <Button className='bg-transparent text-slate-400'>
+                <ArrowRight />
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
 
