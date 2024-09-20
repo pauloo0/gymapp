31f03250e78c598d138edd9f9589e4151942d644
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2024-09-20 20:49:56 +0100

    Style schedules section in client profile

--- src/pages/Client Profile/ClientSchedules.tsx
+++ src/pages/Client Profile/ClientSchedules.tsx
@@ -1,13 +1,49 @@
 import { Schedule } from '@/utils/interfaces'
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
+
 function ClientSchedules({ schedules }: { schedules: Schedule[] }) {
+  const createSchedule = (client_id: string) => {
+    window.location.href = '/marcacoes/nova?client_id=' + client_id
+  }
+
   return (
     <>
-      {schedules.map((schedule) => (
-        <div key={schedule.id}>
-          {schedule.date} - {schedule.time}
-        </div>
-      ))}
+      <Button
+        className='px-4 py-2 mb-6 font-semibold'
+        size='sm'
+        onClick={() => createSchedule(schedules[0].clients.id)}
+      >
+        <Plus className='w-5 h-5 mr-1' /> Adicionar
+      </Button>
+      <Table>
+        <TableHeader>
+          <TableRow></TableRow>
+          <TableRow>
+            <TableHead className='p-2'>Data</TableHead>
+            <TableHead className='p-2'>Hora</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {schedules.map((schedule) => (
+            <TableRow key={schedule.id}>
+              <TableCell className='p-2'>
+                {new Date(schedule.date).toLocaleDateString('pt-PT')}
+              </TableCell>
+              <TableCell className='p-2'>{schedule.time}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </>
   )
 }
