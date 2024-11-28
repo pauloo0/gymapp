02bdc95cdc8713f96f8c4bca97f2514f520fb14b
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2024-11-28 21:49:35 +0000

    Style DB connection error

--- src/pages/DBConnectionError.tsx
+++ src/pages/DBConnectionError.tsx
@@ -8,8 +8,13 @@ function DBConnectionError(props: DBConnectionProps) {
   const { checkConnection } = props
 
   return (
-    <div>
-      <Button size='sm' onClick={checkConnection}>
+    <div className='flex flex-col items-center justify-center h-screen mx-4 space-y-12 overflow-hidden'>
+      <h1 className='text-xl font-bold text-center'>
+        Não consegui estabelecer conexão com a base de dados.
+        <br />
+        Tente novamente mais tarde.
+      </h1>
+      <Button onClick={checkConnection} size={'lg'} className='py-6 text-lg'>
         Tentar novamente
       </Button>
     </div>
