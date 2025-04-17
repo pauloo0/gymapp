eed58ad8dc4985a5a585ce83c48261e9aa26946b
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2025-04-17 12:00:34 +0100

    Fixed not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,22 @@
+import { Button } from '@/components/ui/button'
+
 function NotFound() {
   return (
-    <div>
-      <h1>Page not found.</h1>
-    </div>
+    <main className='flex items-center justify-center min-h-screen'>
+      <div className='flex flex-col gap-6 w-96'>
+        <h1 className='text-2xl font-bold text-center'>
+          Erro 404 - Página não encontrada.
+        </h1>
+        <Button
+          type='button'
+          variant={'secondary'}
+          className='w-full'
+          onClick={() => window.history.back()}
+        >
+          Voltar
+        </Button>
+      </div>
+    </main>
   )
 }
 
